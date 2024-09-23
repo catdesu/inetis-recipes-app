a81740c62bad3d70682d3ead3184f347e494033b
catdesu <david.aeschlimann96@gmail.com>
2024-09-23 13:53:40 +0200

    Add migrations

--- database/migrations/2024_09_23_093907_create_recipes_table.php
+++ database/migrations/2024_09_23_093907_create_recipes_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('fk_users_id');
+            $table->foreign('fk_users_id')->references('id')->on('users');
+            $table->string('title', 50);
+            $table->text('image');
+            $table->integer('prep_time');
         });
     }
 
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipes');
     }
 };
